55e7a9ace4954e3bbbfa3e218904a3fd68ac1990
WesleyChia14 <wchia@unitedvirtualities.com>
2020-01-09 16:45:16 -0600

    added GET users

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 var app = express();
 
+let usersList = [];
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.get('/users', function (req,res) {
+  res.json(usersList);
+});
+
+
 
 
 app.listen(3000, () => {
